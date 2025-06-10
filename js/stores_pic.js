7a09fea092b140b691893609b6d50e5d29f9ece0
goobo251111111 <goobo251111111@gmail.com>
2025-06-10 16:08:18 +0800

    update menu images

--- js/stores_pic.js
+++ js/stores_pic.js
@@ -4,6 +4,18 @@ const mainImage = document.getElementById('mainImage');
 addresses.forEach(address => {
     address.addEventListener('mouseover', () => {
         const newSrc = address.dataset.img;
-        mainImage.src = newSrc;
+    
+
+        if (mainImage.src.includes(newSrc)) return;
+
+        // 先淡出
+        mainImage.style.opacity = '0.34';
+
+        // 換圖並淡入
+        setTimeout(function() {
+            mainImage.src = newSrc;
+            mainImage.style.opacity = '1';
+        }, 250); // 等 0.5 秒再換圖
+
     });
 });
