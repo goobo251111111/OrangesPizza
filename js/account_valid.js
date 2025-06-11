1c82736c8633cfcd8f0bbdf46126c2598ca8650f
goobo251111111 <goobo251111111@gmail.com>
2025-06-11 23:19:06 +0800

    add login validation

---
+++ js/account_valid.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('loginForm');
+
+const accountInput = form.account;
+const passwordInput = form.password;
+
+const errorA = form.querySelector(".errorA");
+const errorP = form.querySelector(".errorP")
+const error2 = form.querySelector('.error2');
+
+const correctAccount = "orange";
+const correctPassword = "0987654321";
+
+
+[accountInput, passwordInput].forEach(input => {
+    input.addEventListener('input', () => {
+        errorA.style.display = 'none';
+        errorP.style.display = 'none';
+        error2.style.display = 'none';
+    });
+});
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let valid = true;
+
+    if (!accountInput.value.trim()) {
+        errorA.style.display = 'block';
+        valid = false;
+    }
+
+    if (!passwordInput.value.trim()) {
+        errorP.style.display = 'block';
+        valid = false;
+    }
+
+    if (!valid) return;
+
+
+    if (
+        accountInput.value.trim() === correctAccount &&
+        passwordInput.value.trim() === correctPassword
+    ) {
+        alert("登入成功！");
+    } else if ( 
+        accountInput.value.trim() && 
+        passwordInput.value.trim()
+    ) {
+        error2.style.display = 'block';
+    }
+});
--- js/stores_pic.js
+++ js/stores_pic.js
@@ -5,17 +5,13 @@ addresses.forEach(address => {
     address.addEventListener('mouseover', () => {
         const newSrc = address.dataset.img;
     
-
         if (mainImage.src.includes(newSrc)) return;
 
-        // 先淡出
         mainImage.style.opacity = '0.34';
 
-        // 換圖並淡入
         setTimeout(function() {
             mainImage.src = newSrc;
             mainImage.style.opacity = '1';
-        }, 250); // 等 0.5 秒再換圖
-
+        }, 250);
     });
 });
